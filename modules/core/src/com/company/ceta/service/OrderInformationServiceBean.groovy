9b11309bb61c29c5b0f653a38a5ec1771b8e0720
Mario David <mariodavid@arcor.de>
2017-02-03 11:39:10 +0100

    added browse button for service; changed OrderInformationServiceBean to database implementation

--- modules/core/src/com/company/ceta/service/OrderInformationServiceBean.groovy
+++ modules/core/src/com/company/ceta/service/OrderInformationServiceBean.groovy
@@ -2,13 +2,30 @@ package com.company.ceta.service
 
 import com.company.ceta.entity.Customer
 import com.company.ceta.entity.Order
+import com.haulmont.cuba.core.global.DataManager
+import com.haulmont.cuba.core.global.LoadContext
 import org.springframework.stereotype.Service
+
+import javax.inject.Inject
 
 @Service(OrderInformationService.NAME)
 public class OrderInformationServiceBean implements OrderInformationService {
 
+    @Inject
+    DataManager dataManager
+
     @Override
     Order findLatestOrder(Customer customer) {
-        customer.orders?.max {it.orderDate}
+
+        def sqlQueryString = 'select e from ceta$Order e where e.customer.id = :customerId order by e.orderDate desc'
+        LoadContext loadContext = LoadContext.create(Order)
+                .setQuery(
+                    LoadContext.createQuery(sqlQueryString)
+                            .setParameter('customerId', customer.id)
+                            .setMaxResults(1)
+                )
+
+
+        dataManager.load(loadContext)
     }
 }
--- modules/web/src/com/company/ceta/web/customer/CustomerBrowse.groovy
+++ modules/web/src/com/company/ceta/web/customer/CustomerBrowse.groovy
@@ -1,6 +1,31 @@
 package com.company.ceta.web.customer
 
+import com.company.ceta.entity.Customer
+import com.company.ceta.entity.Order
+import com.company.ceta.service.OrderInformationService
+import com.haulmont.cuba.gui.WindowManager
 import com.haulmont.cuba.gui.components.AbstractLookup
+import com.haulmont.cuba.gui.components.Frame
+import com.haulmont.cuba.gui.components.Table
+
+import javax.inject.Inject
 
 class CustomerBrowse extends AbstractLookup {
+
+    @Inject
+    Table<Customer> customersTable
+
+    @Inject
+    OrderInformationService orderInformationService
+
+    void findLatestOrder() {
+        Order latestOrder = orderInformationService.findLatestOrder(customersTable.singleSelected)
+
+        if (latestOrder) {
+            openEditor(latestOrder, WindowManager.OpenType.DIALOG)
+        }
+        else {
+            showNotification(getMessage('noOrderFound'), Frame.NotificationType.ERROR)
+        }
+    }
 }
