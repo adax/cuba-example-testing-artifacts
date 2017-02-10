c44c46924fbcbfd45ef72fc4e5de78c35464b55d
Mario David <mariodavid@arcor.de>
2017-02-10 14:18:28 +0100

    fixed OrderInformationServiceIntegrationSpec

--- modules/core/test/com/company/ceta/service/integration/OrderInformationServiceBeanIntegrationSpec.groovy
+++ modules/core/test/com/company/ceta/service/integration/OrderInformationServiceBeanIntegrationSpec.groovy
@@ -1,10 +1,16 @@
 package com.company.ceta.service.integration
 
 import com.company.ceta.entity.Customer
+import com.company.ceta.entity.CustomerType
 import com.company.ceta.entity.Order
 import com.company.ceta.service.OrderInformationService
 import com.company.ceta.service.integration.container.ContainerIntegrationSpec
+import com.haulmont.cuba.core.EntityManager
+import com.haulmont.cuba.core.Transaction
+import com.haulmont.cuba.core.entity.Entity
 import com.haulmont.cuba.core.global.AppBeans
+
+
 /**
  * Created by mario on 03.02.17.
  */
@@ -12,20 +18,43 @@ class OrderInformationServiceBeanSpec extends ContainerIntegrationSpec {
 
     OrderInformationService service
 
+    Customer customer
+
     def setup() {
         service = AppBeans.get(OrderInformationService)
+        customer = new Customer(type: CustomerType.NEW, name: "testCustomer")
     }
 
-    def "test"() {
-        given:
-        Customer customer = metadata.create(Customer)
-        def todaysOrder = metadata.create(Order)
-        customer.setOrders([todaysOrder] as Set)
+    def "findLatestOrder returns the order with the latest orderDate"() {
 
-        when:
+        given: "there are two orders"
+        def todaysOrder = new Order(orderDate: new Date(), customer: customer)
+        def yesterdaysOrder = new Order(orderDate: new Date() - 1, customer: customer)
+
+        and: "the orders belong to that customer"
+        customer.orders = [todaysOrder, yesterdaysOrder] as Set
+
+        and: "the entities are persisted in the database"
+        persist(customer, todaysOrder, yesterdaysOrder)
+
+
+        when: "the latest order of the customer is requested"
         Order latestOrder = service.findLatestOrder(customer)
-        then:
+
+        then: "the latest order is todays order"
         latestOrder == todaysOrder
     }
 
+
+    private void persist(Entity... entitiys) {
+
+        Transaction tx = container.persistence().createTransaction()
+
+        EntityManager entityManager = container.entityManager()
+        entitiys.each {
+            entityManager.persist(it);
+        }
+        tx.commit()
+
+    }
 }
--- modules/core/test/com/company/ceta/service/integration/container/ContainerIntegrationSpec.groovy
+++ modules/core/test/com/company/ceta/service/integration/container/ContainerIntegrationSpec.groovy
@@ -1,8 +1,6 @@
 package com.company.ceta.service.integration.container
 
 import com.haulmont.cuba.core.global.AppBeans
-import com.haulmont.cuba.core.global.DataManager
-import com.haulmont.cuba.core.global.Metadata
 import com.haulmont.cuba.core.sys.DbUpdater
 import spock.lang.Shared
 import spock.lang.Specification
@@ -12,11 +10,11 @@ class ContainerIntegrationSpec extends Specification {
     @Shared
     IntegrationTestContainer container = IntegrationTestContainer.Common.INSTANCE
 
-    @Shared
-    DataManager dataManager
-
-    @Shared
-    Metadata metadata
+//    @Shared
+//    DataManager dataManager
+//
+//    @Shared
+//    Metadata metadata
 
     def setupSpec() {
         container.before()
@@ -30,8 +28,8 @@ class ContainerIntegrationSpec extends Specification {
     }
 
     protected void initBeans() {
-        dataManager = AppBeans.get(DataManager)
-        metadata = AppBeans.get(Metadata)
+//        dataManager = AppBeans.get(DataManager)
+//        metadata = AppBeans.get(Metadata)
     }
 
     def cleanupSpec() {
